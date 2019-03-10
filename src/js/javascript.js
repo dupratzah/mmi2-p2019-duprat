b0cc1943f21ffd407b2a61f7b132c27fee21fb4f
Dupratza <hugo.duprat@laposte.net>
2019-03-10 19:20:16 +0100

    main responsive modificated

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,9 +1,10 @@
+/*LOADING TIME FOR LINK*/
 const btn = document.getElementById("btn");
-
 btn.onclick = e => {
+    event.preventDefault();
     setTimeout(() => {
         
-        window.location = "main.html";
+        window.location = "./../main.html";
 
     }, 2000);
 };
