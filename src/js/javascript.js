23ada4a483259793de5874ab2e7516273f7258eb
Dupratza <hugo.duprat@laposte.net>
2019-03-25 23:17:53 +0100

    rellax fix; articles ok

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,17 +1,69 @@
-/*LOADING TIME FOR LINK*/
-const btn = document.getElementById("btn");
-var largeur = window.innerWidth;
-console.log(largeur);
-btn.onclick = e => {
-    event.preventDefault();
-    if (largeur < 1280) {
-        setTimeout(() => {
-        window.location = "./../main.html";
-        }, 2000);
+import Rellax from "rellax";
+console.log(Rellax);
+
+let btn = document.getElementsByClassName('btn');
+let btn2 = document.getElementsByClassName('btn2')
+let quit = document.getElementsByClassName('quit');
+let choose = document.getElementsByTagName('li');
+const display = document.getElementsByClassName('content')[0];
+const achat = document.getElementsByClassName('achat')[0];
+init();
+
+/*PARRALLAX*/
+var rellax = new Rellax('.star', {
+    center: true
+});
+
+for (let index = 0; index < btn.length; index++) {
+    btn[index].onclick = e =>{
+        affiche();
+    } 
+}
+for (let index = 0; index < btn2.length; index++) {
+    btn2[index].onclick = e =>{
+        affiche2();
+    } 
+}
+for (let index = 0; index < quit.length; index++){
+    quit[index].onclick = e =>{     
+        init();
     }
-    else{
-        setTimeout(() => {
-        window.location = "./../main.html";
-        }, 1500);
+}
+
+
+for (let index = 0; index < choose.length; index++) {
+    choose[index].onclick = e =>{
+        effBorder();
+        setBorder(index);
     }
-};
+    
+}
+function affiche(){
+    display.style.opacity = "0.98";
+    display.style.display = "block";
+}
+function affiche2(){
+    achat.style.opacity = "0.98";
+    achat.style.display = "block";
+}
+function efface(){
+    display.style.display = "none";
+    display.style.opacity = "0";
+}
+function efface2(){
+    achat.style.display = "none";
+    achat.style.opacity = "0";
+}
+function setBorder($index){
+    choose[$index].style.border = "6px solid yellow";
+}
+function effBorder(){
+    for (let index = 0; index < choose.length; index++) {
+        choose[index].style.border = "4px solid black";
+    }
+}
+function init(){
+    efface();
+    efface2();
+    effBorder();
+}
---
+++ src/js/welcome.js
@@ -0,0 +1,18 @@
+  
+/*LOADING TIME FOR LINK*/
+const btn = document.getElementById("btn");
+var largeur = window.innerWidth;
+console.log(largeur);
+btn.onclick = e => {
+    event.preventDefault();
+    if (largeur < 1280) {
+        setTimeout(() => {
+        window.location = "./../main.html";
+        }, 2000);
+    }
+    else{
+        setTimeout(() => {
+        window.location = "./../main.html";
+        }, 1500);
+    }
+};
